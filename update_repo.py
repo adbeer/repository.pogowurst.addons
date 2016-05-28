5b3a2a51d19a319116a6c7bacac22418f1b7ac4e
Steve Hunger <s.hunger@artworked.de>
2016-05-28 18:08:16 +0200

    Initial commit

---
+++ update_repo.py
@@ -0,0 +1,48 @@
+import shutil
+import zipfile
+
+import os
+
+from addons_xml_generator import Generator
+
+if __name__ == "__main__":
+    if __name__ == '__main__':
+        for addon_name in os.listdir('..'):
+            source_addon_dir = os.path.join('..', addon_name)
+            source_addon_xml = os.path.join(source_addon_dir, 'addon.xml')
+            source_addon_icon = os.path.join(source_addon_dir, 'icon.png')
+            target_addon_dir = os.path.join(addon_name)
+            target_addon_xml = os.path.join(target_addon_dir, 'addon.xml')
+            target_addon_icon = os.path.join(target_addon_dir, 'icon.png')
+            target_addon_zip = os.path.join(target_addon_dir, '{0:s}.zip'.format(addon_name))
+
+            if os.path.exists(source_addon_xml):
+                if not os.path.exists(target_addon_dir):
+                    os.mkdir(target_addon_dir)
+                shutil.copy(source_addon_xml, target_addon_xml)
+                if os.path.exists(source_addon_icon):
+                    shutil.copy(source_addon_icon, target_addon_icon)
+                if addon_name.startswith('repository.'):
+                    if os.path.exists(target_addon_zip):
+                        os.unlink(target_addon_zip)
+                    source_addon_dir = addon_name
+                with zipfile.ZipFile(target_addon_zip, 'w', zipfile.ZIP_DEFLATED) as zip:
+                    for root, dirs, files in os.walk(source_addon_dir):
+                        for f in files:
+                            filename = os.path.join(root, f)
+                            if source_addon_dir == addon_name:
+                                arcname = filename
+                            else:
+                                arcname = os.path.relpath(filename, os.path.join('..'))
+
+                            if f in [
+                                'addon.py',
+                                'addon.xml',
+                                'changelog.txt',
+                                'fanart.jpg',
+                                'icon.png',
+                                'LICENSE.txt'
+                            ] or arcname.startswith(os.path.join(addon_name, 'resources')):
+                                zip.write(filename, arcname)
+
+        Generator()
